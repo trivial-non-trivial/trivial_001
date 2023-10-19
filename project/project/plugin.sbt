5816c220c73527c193e424046be3cffd4592104c
david@kaplone.fr <david@kaplone.fr>
2023-10-20 01:13:41 +0200

    trivial http page

---
+++ project/project/plugin.sbt
@@ -0,0 +1,3 @@
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.9.1")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.10")
+addSbtPlugin("com.lihaoyi" % "workbench" % "0.4.0")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    ScalatagsServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
